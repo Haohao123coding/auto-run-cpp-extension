0ac8f97f1dcc5d4cc242db2dfc108761386a640e
Haohao123coding <18217288892@163.com>
2025-10-25 14:49:54 +0800

    add run and comprun

--- src/main.js
+++ src/main.js
@@ -1,33 +1,77 @@
 const vscode = require('vscode');
 const path = require('path');
 
+function compileCommand(terminal){
+    const editor = vscode.window.activeTextEditor;
+    const document = editor.document;
+    const filePath = document.fileName;
+    const additionalCompileCommand = "-std=c++14 -O2 -static";
+
+    vscode.window.showInformationMessage('正在编译 C++ 文件...');
+
+    // get directory and output path
+    const fileDir = path.dirname(filePath);
+    const fileNameWithoutExt = path.basename(filePath, path.extname(filePath));
+    const outputPath = path.join(fileDir, fileNameWithoutExt);
+
+    // setup terminal
+    terminal.sendText(`cd "${fileDir}"`);
+
+    // compile
+    const compileCommand = `g++ "${filePath}" -o "${outputPath}" ${additionalCompileCommand}`;
+    terminal.sendText(compileCommand);
+    terminal.show();
+}
+
+function runCommand(terminal){
+    const editor = vscode.window.activeTextEditor;
+    const document = editor.document;
+    const filePath = document.fileName;
+
+    vscode.window.showInformationMessage('正在运行 C++ 文件...');
+
+    // get directory and output path
+    const fileDir = path.dirname(filePath);
+    const fileNameWithoutExt = path.basename(filePath, path.extname(filePath));
+    const exeutablePath = path.join(fileDir, fileNameWithoutExt);
+
+    // setup terminal
+    terminal.sendText(`cd "${fileDir}"`);
+
+    // run
+    const runCommand = `"${exeutablePath}"`;
+    terminal.sendText(runCommand);
+    terminal.show();
+}
+
 function activate(context){
     console.log('C++ 运行扩展已被激活');
 
-    // 注册命令
-    let disposable = vscode.commands.registerCommand('auto-run-cpp.compileCommand', () => {
-        const editor = vscode.window.activeTextEditor;
-        const document = editor.document;
-        const filePath = document.fileName;
-        const additionalCompileCommand = "-std=c++14 -O2 -static";
-
-        vscode.window.showInformationMessage('正在编译 C++ 文件...');
-
-        // get directory and output path
-        const fileDir = path.dirname(filePath);
-        const fileNameWithoutExt = path.basename(filePath, path.extname(filePath));
-        const outputPath = path.join(fileDir, fileNameWithoutExt);
-
+    // command to compile C++ file
+    let disposableCompile = vscode.commands.registerCommand('auto-run-cpp.compileCommand', () => {
         // setup terminal
-        const terminal = vscode.window.createTerminal('C++ Compiler');
-        terminal.sendText(`cd "${fileDir}"`);
-
-        // compile
-        const compileCommand = `g++ "${filePath}" -o "${outputPath}" -${additionalCompileCommand}`;
-        terminal.sendText(compileCommand);
+        const terminal = vscode.window.createTerminal('C++');
+        compileCommand(terminal);
     });
 
-    context.subscriptions.push(disposable);
+    // command to run C++ file
+    let disposableRun = vscode.commands.registerCommand('auto-run-cpp.runCommand', () => {
+        // setup terminal
+        const terminal = vscode.window.createTerminal('C++');
+        runCommand(terminal);
+    });
+
+    // command to compile and run C++ file
+    let disposableCompileRun = vscode.commands.registerCommand('auto-run-cpp.compileAndRunCommand', () => {
+        // setup terminal
+        const terminal = vscode.window.createTerminal('C++');
+        compileCommand(terminal);
+        runCommand(terminal);
+    });
+
+    context.subscriptions.push(disposableCompile);
+    context.subscriptions.push(disposableRun);
+    context.subscriptions.push(disposableCompileRun);
 }
 
 module.exports = {
